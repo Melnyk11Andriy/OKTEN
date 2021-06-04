0c8d9ea36b5610a6a9c387d6ad2b56155c04c066
Melnyk Andriy <a_melnica@yahoo.com>
2021-06-04 19:06:23 +0300

    ucking test

--- ANGULAR/angular-second/src/app/components/index.components.ts
+++ ANGULAR/angular-second/src/app/components/index.components.ts
@@ -5,3 +5,5 @@ export * from './post/post.component';
 export * from './user-details/user-details.component';
 export * from './post-details/post-details.component';
 export * from './comments/comments.component';
+
+//______________
